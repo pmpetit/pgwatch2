02227ae230d303f7edf8ef0f46cc6ba73de3fbaa
Pierre-Marie Petit <pierremarie.petit.partner@decathlon.com>
2021-04-02 17:48:48 +0200

    Update metric.sql
    problem on different tables with same idx name:
    for example 
    t1 with my_idx
    t2 with my_idx 
    will duplicate rows in results

--- pgwatch2/metrics/reco_partial_index_candidates/9.0/metric.sql
+++ pgwatch2/metrics/reco_partial_index_candidates/9.0/metric.sql
@@ -1,16 +1,15 @@
 select distinct
     (extract(epoch from now()) * 1e9)::int8 as epoch_ns,
     'partial_index_candidates'::text as tag_reco_topic,
-    quote_ident(ni.nspname)||'.'||quote_ident(ci.relname) as tag_object_name,
-    ('index ' || quote_ident(ni.nspname)||'.'||quote_ident(ci.relname) || ' on ' || quote_ident(s.schemaname) || '.' || quote_ident(s.tablename) || ' column ' || quote_ident(s.attname)  || ' could possibly be declared partial leaving out NULL-s')::text as recommendation,
+    quote_ident(ni.nspname)||'.'||quote_ident(c.relname) as tag_object_name,
+    ('index ' || quote_ident(ni.nspname)||'.'||quote_ident(c.relname) || ' on ' || quote_ident(s.schemaname) || '.' || quote_ident(s.tablename) || ' column ' || quote_ident(s.attname)  || ' could possibly be declared partial leaving out NULL-s')::text as recommendation,
     'NULL fraction: ' || round((null_frac * 100)::numeric, 1) || '%, rowcount estimate: ' || (c.reltuples)::int8 || ', current definition: ' ||  pg_get_indexdef(i.indexrelid) as extra_info
 from
     pg_stats s
     join pg_attribute a using (attname)
     join pg_index i on i.indkey[0] = a.attnum and i.indrelid = a.attrelid
     join pg_class c on c.oid = i.indrelid
-    join pg_class ci on ci.oid = i.indexrelid
-    join pg_namespace ni on ni.oid = ci.relnamespace
+    join pg_namespace ni on ni.oid = c.relnamespace
 where
   not indisprimary
   and not indisunique
